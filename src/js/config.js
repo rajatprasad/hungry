708cf66a71d796fd060a429f44324143a14714da
rajat prasad <rajatprasad@rajats-MacBook-Air.local>
2022-07-23 12:38:57 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 6;
+export const KEY = '32d9dbd4-711d-4467-9877-29dd3cbce8fa';
+export const MODAL_CLOSE_SEC = 2.5;
